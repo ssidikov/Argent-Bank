6032043a8e8ec30d61fd2fb76ee8b7f9644349e0
Sardorbek SIDIKOV <sardorbek.sidikov7@gmail.com>
2024-11-16 13:39:39 +0100

    init with backend

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import './Home.sass'
+
+function Home() {
+  return (
+    <section className='home'>
+      <h1 className='home__text'>Welcome to home</h1>
+      <div className='home__content'>
+        <p className='home__content-text'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptate. Quisquam,
+          voluptate.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Home
