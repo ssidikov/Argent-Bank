9048a58d3f911405121116697bb92020fdb457de
Sardorbek SIDIKOV <sardorbek.sidikov7@gmail.com>
2024-11-18 08:53:58 +0100

    Add Login page and LoginForm component with styles

---
+++ frontend/src/components/LoginForm/index.jsx
@@ -0,0 +1,33 @@
+import UserIcon from '../../assets/user-icon.svg'
+import './LoginForm.sass'
+import { Link } from 'react-router-dom'
+
+function LoginForm() {
+  return (
+    <section className='login-container'>
+      <img src={UserIcon} alt='User Icon' className='login-icon' />
+      <h1 className='login-title'>Sign In</h1>
+      <form>
+        <div className='input-wrapper'>
+          <label htmlFor='email'>Username</label>
+          <input type='email' id='email' name='email' />
+        </div>
+        <div className='input-wrapper'>
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' name='password' />
+        </div>
+        <div className='input-remember'>
+          <input type='checkbox' id='remember-me'></input>
+          <label for='remember-me'>Remember me</label>
+        </div>
+        <Link to='profile'>
+          <button type='submit' className='login-button'>
+            Sign In
+          </button>
+        </Link>
+      </form>
+    </section>
+  )
+}
+
+export default LoginForm
---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,12 @@
+import './Login.sass'
+import LoginForm from '../../components/LoginForm'
+
+function Login() {
+  return (
+    <main className='login'>
+      <LoginForm />
+    </main>
+  )
+}
+
+export default Login
