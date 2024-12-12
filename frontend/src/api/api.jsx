a1d0a39ae37abbdd7b6160dbd34e7ad7003e32fb
Sardorbek SIDIKOV <sardorbek.sidikov7@gmail.com>
2024-12-12 13:27:59 +0100

    API functions to improve user authentication and account fetching logic

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,6 +1,6 @@
-// api.jsx
 import axios from 'axios'
 
+// Create an axios instance with base URL and headers
 const api = axios.create({
   baseURL: 'http://localhost:3001/api/v1',
   headers: {
@@ -8,8 +8,10 @@ const api = axios.create({
   },
 })
 
+// Function to log in a user
 export const loginUser = async (email, password) => {
   try {
+    // Send POST request for user login
     const response = await api.post('/user/login', { email, password })
     return response.data
   } catch (error) {
@@ -17,8 +19,10 @@ export const loginUser = async (email, password) => {
   }
 }
 
+// Function to get user information
 export const getUserInfo = async (token) => {
   try {
+    // Send POST request to fetch user profile with an empty request body
     const response = await api.post(
       '/user/profile',
       {},
@@ -32,10 +36,11 @@ export const getUserInfo = async (token) => {
   }
 }
 
+// Function to update user name
 export const updateUserNameAPI = async (token, profileData) => {
   try {
     const response = await api.put('/user/profile', profileData, {
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${token}` }, // Authorization token
     })
     return response.data
   } catch (error) {
@@ -43,12 +48,16 @@ export const updateUserNameAPI = async (token, profileData) => {
   }
 }
 
+// Additional function to fetch user accounts data
+
 export const getUserAccounts = async (token, userId) => {
   try {
     const response = await axios.get(
       'https://raw.githubusercontent.com/ssidikov/Argent-Bank/refs/heads/master/frontend/src/data/accounts.json'
     )
+    // Filter data to find accounts for the given user ID
     const accountsData = response.data.filter((user) => user.userId === userId)
+    // Check if accounts are found for the given user ID
     if (accountsData.length === 0) throw new Error('Accounts not found for the given user ID')
     return accountsData[0].accounts
   } catch (error) {
